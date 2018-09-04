593efebc143cbec58e78ccf3f15cdf2b4af37a43
Diógenes Augusto Fernandes Hermínio <diofeher@gmail.com>
2018-09-04 00:31:32 -0300

    missing integer solution

--- codility/4_missing_integer.py
+++ codility/4_missing_integer.py
@@ -1,10 +1,10 @@
 def solution(A):
     missing = 1
-    for i in range(max(A) + 2):
-        if i > 0:
-            if i not in A:
-                missing = i
-                break
+    for elem in sorted(A):
+        if elem == missing:
+            missing += 1
+        if elem > missing:
+            break
     return missing
 
 assert solution([1,3,6,4,1,2]) == 5
